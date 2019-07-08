8db00f06dc2285d08350fb77667ec9422e0b26dc
Ghasak Ibrahim <mdotgh@gmail.com>
2019-07-08 23:40:00 +0900

    Finish First Graphical Addtion TensorFlow Algorithm

--- src/02/addition.py
+++ src/02/addition.py
@@ -8,14 +8,14 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # Define computational graph
 
-X = tf.placeholder(tf.float32, name = 'X')  #Placholder Node
+X = tf.compat.v1.placeholder(tf.float32, name = 'X')  # Placholder Node
 
-Y = tf.placeholder(tf.float32, name = 'Y')
+Y = tf.compat.v1.placeholder(tf.float32, name = 'Y')
 
 addtion = tf.add(X,Y, name = 'addition') # Add the nodes
 
 # Create the session
-with tf.Session() as session:
-    result =  session.run(addtion, feed_dict = {X: [1], Y: [4]})
+with tf.compat.v1.Session() as session:
+    result =  session.run(addtion, feed_dict = {X: [1, 2, 10], Y: [4, 2, 10]})
 
     print(result)
